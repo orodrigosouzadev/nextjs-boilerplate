96e2a9adb30682500f6ee607a8b44eaa1b06bd70
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2023-07-26 15:47:27 -0300

    chore: configuring pwa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -40,7 +40,9 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="pt-BR">
-        <Head />
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+        </Head>
         <body>
           <Main />
           <NextScript />
