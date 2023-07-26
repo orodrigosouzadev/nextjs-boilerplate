dbcf4de4131b7d40017d35868665a09abb0c9a5f
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2023-07-26 15:59:59 -0300

    chore: configure test coverage

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,14 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/styles/**',
+    '!src/pages/_app.tsx',
+    '!src/pages/_document.tsx',
+    '!src/pages/index.tsx',
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   moduleNameMapper: {
     '^styled-components':
